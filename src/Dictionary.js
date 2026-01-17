4a6d643649b224bbec8d73ac8c1764dabd0163b4
d-detweiler <d.s.detweiler@gmail.com>
2026-01-16 16:57:44 -0800

    Added dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data.meanings[0]);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`searching for ${keyword} definition`);
+
+    let apiKey = "0f1t882aaea024e3o0b330af83a5fb90";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
